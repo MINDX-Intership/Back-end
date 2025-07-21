3fca257c4c495b517c50ab29edd8f5d6e1e9051a
nhad1702 <anhducnhsg@gmail.com>
2025-07-21 15:32:14 +0700

    final commit

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ import './models/TaskComments.Models.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: process.env.CLIENT_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức HTTP được phép
+    allowedHeaders: ['Content-Type', 'Authorization'], // Các header được phép
+  }
+));
 
 
 // Routes
