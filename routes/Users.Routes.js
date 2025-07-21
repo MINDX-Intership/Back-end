169e27730296ca36ca897e521de0a5b66a9ac3f9
lexuanbaonam <lexuanbaonam2008@gmail.com>
2025-07-21 14:47:02 +0700

    CN002-CN003

--- routes/Users.Routes.js
+++ routes/Users.Routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import userController from '../controllers/User.Controllers.js';
-// import { authVerify } from '../middlewares/Auth.Middlewares.js';
+import { authVerify } from '../middlewares/Auth.Middlewares.js';
 // import { protect, authorize } from '../middlewares/Auth.Middlewares.js';
 
 const userRouter = express.Router();
 
 // Lấy thông tin tài khoản đã login
-// userRouter.get('/me', protect, (req, res) => {
-//   res.json({
-//     email: req.account.email,
-//     role: req.account.role,
-//     verified: req.account.isVerified
-//   });
-// });
+userRouter.get('/me', protect, (req, res) => {
+  res.json({
+    email: req.account.email,
+    role: req.account.role,
+    verified: req.account.isVerified
+  });
+});
 
 // Lấy thông tin cá nhân
 userRouter.get('/me', authenticateToken, userController.getUser);
@@ -22,8 +22,8 @@ userRouter.put('/me', authenticateToken, userController.updateUser);
 
 
 // // route role cho admin
-// userRouter.get('/admin-only', protect, authorize('ADMIN'), (req, res) => {
-//   res.json({ message: 'Chỉ ADMIN mới truy cập được route này.' });
-// });
+userRouter.get('/admin-only', protect, authorize('ADMIN'), (req, res) => {
+  res.json({ message: 'Chỉ ADMIN mới truy cập được route này.' });
+});
 
 export default userRouter;
