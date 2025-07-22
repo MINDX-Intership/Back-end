7d2a74d597b4dfc700a4bd7ee140559276b1d77d
nhad1702 <anhducnhsg@gmail.com>
2025-07-22 15:07:49 +0700

    add job position and depart

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ import accountRouter from './Accounts.Routes.js';
 import userRouter from './Users.Routes.js';
 import sprintRouter from './Sprints.Routes.js';
 import taskRouter from './Tasks.Routes.js';
+import jobPositionRouter from './JobPositions.Routes.js';
+import departRouter from './Departs.Routes.js';
 
 
 const router = express.Router();
@@ -11,5 +13,7 @@ router.use('/account', accountRouter);
 router.use('/user', userRouter);
 router.use('/sprint', sprintRouter);
 router.use('/task', taskRouter);
+router.use('/job-position', jobPositionRouter);
+router.use('/depart', departRouter);
 
 export default router;
