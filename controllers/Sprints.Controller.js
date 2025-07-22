0b6cc0dc56a466998aa08a19b7ced00688f2c890
lexuanbaonam <lexuanbaonam2008@gmail.com>
2025-07-22 15:44:32 +0700

    B08 - HT09

--- controllers/Sprints.Controller.js
+++ controllers/Sprints.Controller.js
@@ -1,7 +1,7 @@
-import { get } from "mongoose";
 import sprintModel from "../models/Sprints.Models.js";
 
 const sprintController = {
+    // Tạo Sprint
     createSprint: async (req, res) => {
         const { title, describe, startDate, endDate } = req.body;
 
@@ -24,6 +24,8 @@ const sprintController = {
             res.status(500).json({ message: "Lỗi khi tạo sprint", error: err.message });
         }
     },
+
+    // Lấy các Sprint đã tạo
     getSprint: async (req, res) => {
         try {
             const sprints = await sprintModel.find({ createdBy: req.user.id });
@@ -31,7 +33,27 @@ const sprintController = {
         } catch (err) {
             res.status(500).json({ message: "Lỗi khi lấy danh sách sprint", error: err.message });
         }
+    },
+
+    // Xóa Sprint
+    deleteSprint: async (req, res) => {
+        try {
+            const sprint = await sprintModel.findById(req.params.id);
+
+            if (!sprint) {
+                return res.status(404).json({ message: "Sprint không tồn tại." });
+            }
+
+            if (sprint.createdBy.toString() !== req.user.id) {
+                return res.status(403).json({ message: "Bạn không có quyền xóa sprint này." });
+            }
+
+            await sprint.deleteOne();
+            res.status(200).json({ message: "Xóa sprint thành công." });
+        } catch (err) {
+            res.status(500).json({ message: "Lỗi khi xóa sprint", error: err.message });
+        }
     }
-}
+};
 
 export default sprintController;
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ import accountRouter from './Accounts.Routes.js';
 import userRouter from './Users.Routes.js';
 import sprintRouter from './Sprints.Routes.js';
 import taskRouter from './Tasks.Routes.js';
-
-
+import notificationRouter from './Notifications.Routes.js'; 
+import documentRouter from './Documents.Routes.js';          
 const router = express.Router();
 
 router.use('/account', accountRouter);
 router.use('/user', userRouter);
 router.use('/sprint', sprintRouter);
 router.use('/task', taskRouter);
+router.use('/notifications', notificationRouter); 
+router.use('/documents', documentRouter);         
 
 export default router;
