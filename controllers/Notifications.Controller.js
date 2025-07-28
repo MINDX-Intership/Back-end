b723be7da5dca50f0e20bc2a1138c48ba2448fa8
lexuanbaonam <lexuanbaonam2008@gmail.com>
2025-07-28 17:09:49 +0700

    update

--- controllers/Notifications.Controller.js
+++ controllers/Notifications.Controller.js
@@ -7,10 +7,10 @@ export const getNewNotifications = async (req, res) => {
 
 export const markAsRead = async (req, res) => {
   await Notification.updateMany({ userId: req.user._id }, { isRead: true });
-  res.json({ message: 'Marked all as read' });
+  res.json({ message: 'Tất cả thông báo đã được đọc.' });
 };
 
 export const deleteNotifications = async (req, res) => {
   await Notification.deleteMany({ userId: req.user._id });
-  res.json({ message: 'Deleted all notifications' });
+  res.json({ message: 'Xóa tất cả thông báo.' });
 };
